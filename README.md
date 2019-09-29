de109bf1ba5fd293240be01ce7b0bcc5833c07c0
Norman Malessa <mail@norman-malessa.de>
2019-09-29 09:49:33 +0200

    initial release

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# plg_aiswembed
-Embed videos from aisw.tv easily into your webpage
+# plg_soccerwatchembed
+
+Embed videos from soccerwatch.tv / aisw.tv easily into your website.
+
+## Requirements
+
+- Joomla ^3.0
+
+## Installation
+
+Download `zip`-file and install it using the Joomla-Installer.
+This will install as a content-plugin.
+
+## Usage
+
+Simply write the url to the desired video into your article in one of the following forms:
+```
+https://soccerwatch.tv/embed/33422/highlight
+{ https://soccerwatch.tv/embed/33422/highlight }
+```
+Note: You have to use the `/embed/`- url.
---
+++ soccerwatchembed.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * @package PlgSoccerwatchEmbed
+ * @author Norman Malessa <mail@norman-malessa.de>
+ * @copyright 2019 Norman Malessa <mail@norman-malessa.de>
+ * @license http://www.opensource.org/licenses/mit-license.php The MIT License, see LICENSE
+ */
+
+class plgContentSoccerwatchEmbed extends JPlugin
+{
+    CONST REGEX = '/{?\s*(https?:\/\/(?:www\.)?soccerwatch\.tv\/embed\/([0-9]+)\/highlight)\/?\s*}?/';
+
+    /**
+     * @param string $context
+     * @param object $article
+     * @return bool
+     */
+    public function onContentPrepare($context, $article)
+    {
+        if ($context === 'com_content.article')
+        {
+            $article->text = preg_replace(self::REGEX, $this->getReplacement(), $article->text);
+        }
+
+        return true;
+    }
+
+    /**
+     * @return string
+     */
+    private function getReplacement()
+    {
+        $width = $this->params->get('width', '100%');
+        $height = $this->params->get('height', '500');
+
+        return '<iframe id="soccerwatch_$2" frameborder="0" allowfullscreen'
+            . ' width="' . $width . '" height="' . $height . '" src="$1"></iframe>';
+    }
+}
